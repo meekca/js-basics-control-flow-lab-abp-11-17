435a2a0a163d5330f85b5df1cd8f5adc6b8f2fe5
Cameron Meek <meekca1@gmail.com>
2018-03-14 22:51:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+function scuberGreetingForFeet() {
+  if (feet === 199) {
+    return "This one is on me!";
+  }
+  else if (feet === 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
+}
